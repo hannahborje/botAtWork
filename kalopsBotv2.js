c4acebf095dc2dda35c56479f7bac6fab9336d73
Hannah Börjesson <hannah.borjesson@gmail.com>
2017-04-02 17:32:33 +0200

    remove unnecessary code

--- kalopsBotv2.js
+++ kalopsBotv2.js
@@ -252,18 +252,6 @@ controller.hears(['håll käften!'], 'direct_message,direct_mention,mention', fu
     });
 });
 
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-});
-
 function setDepartureList (json) {
 
     var journeysList = json["soap:Envelope"]["soap:Body"]["GetJourneyResponse"]["GetJourneyResult"]["Journeys"]["Journey"];
@@ -308,22 +296,4 @@ function setDepartureList (json) {
     } else {
         return "Nått blev knas"
     }
-}
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
 }
